51fdcfdccd57dcc6723425a07657d4f12fe263e8
alexsilva75 <alexsandro.ssantos75@gmail.com>
2022-02-04 11:48:21 -0300

    Criando relacionamentos

---
+++ app/Models/Store.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Store extends Model
+{
+    use HasFactory;
+    protected $fillable = ['name', 'description', 'phone', 'mobile_phone', 'slug'];
+
+    public function user()
+    {
+        $this->belongsTo(User::class);
+    }
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,65 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/model', function () {
+    // $products = \App\Models\Product::all();
+
+    // return $products;
+
+    // $user = \App\Models\User::find(10);
+    // $store = $user->store()->create([
+    //     'name' => 'Loja Teste',
+    //     'description' => 'loja Teste de produtos de informatica',
+    //     'phone' => '77-7777-7777',
+    //     'mobile_phone' => '77-77777-7777',
+    //     'slug' => 'loja-teste'
+    // ]);
+
+
+
+    // $store = \App\Models\Store::find(41);
+
+    // return $store->products()->create([
+    //     'name' => 'Notebook Dell',
+    //     'description' => 'CORE i5 12GB RAM',
+    //     'body' => 'whatever...',
+    //     'price' => 7000,
+    //     'slug' => 'notebook-dell',
+    // ]);
+
+    // $categories = [];
+
+    // $category = \App\Models\Category::create([
+    //     'name' => 'Computadores',
+    //     'description' => null,
+    //     'slug' => 'computadores',
+
+    // ]);
+
+    // array_push($categories, $category);
+
+    // $category = \App\Models\Category::create([
+    //     'name' => 'Games',
+    //     'description' => null,
+    //     'slug' => 'games',
+
+    // ]);
+    // array_push($categories, $category);
+
+
+    // $category = \App\Models\Category::create([
+    //     'name' => 'Notebooks',
+    //     'description' => null,
+    //     'slug' => 'notebooks',
+
+    // ]);
+    // array_push($categories, $category);
+
+    $product = \App\Models\Product::find(41);
+
+    $product->categories()->attach([3]);
+    $product->categories()->detach([3]);
+    // $product->categories()->sync([3]);
+    return $product->categories;
+});
